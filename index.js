99f79b81417050855dec6e95f3b950cb340d4d90
Daniel Cuello <danielbj38@gmail.com>
2023-11-09 10:11:44 -0300

    Actualizado

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ db.authenticate()
   .catch((error) => console.log(error));
 
 // Definir puerto
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 // Habilitar Pug
 
